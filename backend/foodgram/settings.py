c9bf5aaa40a47fd44917641983bb083e2afc8cfc
Gerartg <aptypob@mail.ru>
2021-12-09 13:58:45 +0400

    test workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,9 +5,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your_secret_key')
 
